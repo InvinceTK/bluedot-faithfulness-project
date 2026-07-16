2ac1706dcf0595461d816145c93c766e03f05dc8
Elliot <elliotkaute@gmail.com>
2026-07-16 15:59:37 +0100

    ab

--- scripts/cf_gen/llm_cf_gen.sh
+++ scripts/cf_gen/llm_cf_gen.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 GPU=0
 MODEL=Qwen/Qwen3-1.7B
 DATASET=zebra_logic
-IN=data/natural_counterfactuals/compas_counterfactual_dataset_balanced_10.parquet
-OUT=parquet/tests/compas_cf_80.parquet
+
+IN=data/natural_counterfactuals/1_heart_disease_counterfactual_dataset_balanced.parquet
+OUT=parquet/tests/heart_disease_1.parquet
 
 CUDA_VISIBLE_DEVICES=$GPU python -m src.counterfactual_generation.llm_counterfactual_generation.generate_counterfactuals \
     "$IN" \
